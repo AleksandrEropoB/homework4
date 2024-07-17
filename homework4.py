880d8c11ec6a6e90e75965e6647b68430d0fb105
AleksandrEropoB <onespirit@yandex.ru>
2024-07-17 18:48:17 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+mi_string = input('Введите Ваш текст: ')
+print('в вашем тексте',len(mi_string), 'символов')
+print(mi_string. upper())
+print(mi_string. lower())
+print(mi_string. replace(' ', ''))
+print(mi_string[0])
+print(mi_string[-1])
+
